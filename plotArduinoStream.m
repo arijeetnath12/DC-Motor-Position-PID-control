33e19085188550b9707d8bb4855a7b2d41e68331
Arijeet Nath <94764537+arijeetnath12@users.noreply.github.com>
2025-09-03 00:17:25 -0400

    Update plotArduinoStream.m

--- plotArduinoStream.m
+++ plotArduinoStream.m
@@ -6,17 +6,19 @@ configureTerminator(s,"CR/LF");
 flush(s);
 
 f = figure('Name','Velocity'); hold on; grid on;
-hRef = animatedline('DisplayName','setPoint');
-hPos = animatedline('DisplayName','position');
+hRef = animatedline('DisplayName','ref','Color',[0,0,1]);
+hPos = animatedline('DisplayName','y','Color',[1,0,0]);
 legend show
-xlabel('Time(s)'), ylabel('revolutions'); t0 = tic;
+xlabel('Time(s)'), ylabel('counts'); t0 = tic;
+
 
 while isvalid(hRef) && isvalid(hPos)
     ln = readline(s);
     v = sscanf(ln,'%f');
     if numel(v) == 2
-        ref = v(1);
-        y = v(2);
+        sgn = sign(v(1)); if sgn == 0, sgn =1; end
+        ref = sgn*v(1);
+        y = sgn*v(2);
         
         t = toc(t0);
         addpoints(hRef,t,ref);
